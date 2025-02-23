f612e9be06756d568b0f753fb533f8ee3e2296c1
Jonatã Oliveira <jonata.96ol@gmail.com>
2025-02-23 11:03:50 -0300

    feat: ✨ sync level between users and publishers

--- set-level.js
+++ set-level.js
@@ -1,4 +1,4 @@
-import { Client, Query, Databases } from 'node-appwrite'
+import { Client, Query, Databases, Users } from 'node-appwrite'
 
 const client = new Client()
 
@@ -8,34 +8,67 @@ client
 	.setKey(process.env.APPWRITE_API_KEY)
 
 const databases = new Databases(client)
+const users = new Users(client)
 
 export default async ({ req, res, log, error }) => {
 	try {
 		log(req)
-		// // Get the event data from the request
-		// const payload = req.body
+		// User update event
+		if (req.headers['x-appwrite-event'].includes('users')) {
+			// Get the event data from the request
+			const payload = req.body
 
-		// // Check if this is a user update event and involves labels
-		// if (!payload.labels) {
-		// 	return res.send('Not a relevant user update event', 200)
-		// }
+			// Check if this is a user update event and involves labels
+			if (!payload.labels) {
+				return res.send('Not a relevant user update event', 200)
+			}
 
-		// // Check if 'admin' label was added or removed
-		// const adminAdded = payload.labels.includes('admin')
+			// Check if 'admin' label was added or removed
+			const adminAdded = payload.labels.includes('admin')
 
-		// // Find the publisher in production database
-		// const publisher = await databases.listDocuments('production', 'publishers', [Query.equal('user', payload.$id)])
+			// Find the publisher in production database
+			const publisher = await databases.listDocuments('production', 'publishers', [
+				Query.equal('user', payload.$id),
+			])
 
-		// if (publisher.documents.length === 0) {
-		// 	return res.send('No publisher found for this user', 200)
-		// }
+			if (publisher.documents.length === 0) {
+				return res.send('No publisher found for this user', 200)
+			}
 
-		// // Update publisher level
-		// const newLevel = adminAdded ? 1 : 3
+			// Update publisher level
+			const newLevel = adminAdded ? 1 : 3
 
-		// await databases.updateDocument('production', 'publishers', publisher.documents[0].$id, { level: newLevel })
+			await databases.updateDocument('production', 'publishers', publisher.documents[0].$id, { level: newLevel })
 
-		// return res.send(`Publisher level updated to ${newLevel}`, 200)
+			return res.send(`Publisher level updated to ${newLevel}`, 200)
+		}
+
+		// Publisher update event
+		if (req.headers['x-appwrite-event'].includes('databases.production.collections.publishers.documents')) {
+			// Get the event data from the request
+			const payload = req.body
+
+			// Check if this is a publisher update event and involves level
+			if (!payload.level) {
+				return res.send('Not a relevant publisher update event', 200)
+			}
+
+			// Update user level
+			const user = await users.get(payload.user)
+
+			if (payload.level === 1) {
+				// Add 'admin' label to the user
+				await users.updateLabels(user.$id, [...user.labels.filter((label) => label !== 'admin'), 'admin'])
+			} else {
+				// Remove 'admin' label from the user
+				await users.updateLabels(
+					user.$id,
+					user.labels.filter((label) => label !== 'admin')
+				)
+			}
+
+			return res.send(`User level updated to ${payload.level}`, 200)
+		}
 	} catch (exception) {
 		error(exception)
 		return res.send('Set level failed, please try again later.', 500)
